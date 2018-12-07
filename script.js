8e583578a8c05b5f9c7d8e91071057f162623e75
cmacswan07 <cmacswan07@gmail.com>
2018-12-07 09:17:44 -0800

    Added more breakpoints. The image is spilling over the header section into the about section, so I'll need to fix that next.

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ const app = new Vue({
 		projects: [
 			{ name: '/shopping_list/', description: 'A shopping list tool made with Vue JS.', link: './shopping_list/' },
 			{ name: '/my_library/', description: 'A tool to help keep track of a library of books.', link: './my_library/' },
-			{ name: '/tic_tac_toe/', description: 'Tic-tac-toe game. Vanilla JS.', link: './tic-tac-toe/' },
+			{ name: '/tic_tac_toe/', description: 'Tic-tac-toe game. Vanilla JS.', link: './tic_tac_toe/' },
 			{ name: '/calculator/', description: 'Simple calculator. Vanilla JS.', link: './calculator/' },
 			{ name: '/framework_project/', description: 'CSS grid framework.', link: './framework_project/' },
 			{ name: '/etch-a-sketch/', description: 'A browser etch-a-sketch made with vanilla JS.', link: './etch-a-sketch/'}
