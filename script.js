a6d2b1093691527e680d3e462fb48fd20a8bb490
cmacswan07 <cmacswan07@gmail.com>
2018-12-06 19:05:20 -0800

    Working on making it mobile friendly now

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ const app = new Vue({
 			{ name: '/my_library/', description: 'A tool to help keep track of a library of books.', link: './my_library/' },
 			{ name: '/tic_tac_toe/', description: 'Tic-tac-toe game. Vanilla JS.', link: './tic-tac-toe/' },
 			{ name: '/calculator/', description: 'Simple calculator. Vanilla JS.', link: './calculator/' },
+			{ name: '/framework_project/', description: 'CSS grid framework.', link: './framework_project/' },
 			{ name: '/etch-a-sketch/', description: 'A browser etch-a-sketch made with vanilla JS.', link: './etch-a-sketch/'}
 		]
 	}
