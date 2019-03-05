cbec961252bf60a03458bd68ddf07315d237364f
Chris MacSwan <cmacswan07@gmail.com>
2019-03-04 18:19:11 -0800

    Revamped styling.

--- script.js
+++ script.js
@@ -9,12 +9,13 @@ const app = new Vue({
 	el: '#app',
 	data: {
 		projects: [
-			{ name: '/shopping_list/', description: 'A shopping list tool made with Vue JS.', link: './shopping_list/' },
-			{ name: '/my_library/', description: 'A tool to help keep track of a library of books.', link: './my_library/' },
-			{ name: '/tic_tac_toe/', description: 'Tic-tac-toe game. Vanilla JS.', link: './tic_tac_toe/' },
-			{ name: '/calculator/', description: 'Simple calculator. Vanilla JS.', link: './calculator/' },
-			{ name: '/framework_project/', description: 'CSS grid framework.', link: './framework_project/' },
-			{ name: '/etch-a-sketch/', description: 'A browser etch-a-sketch made with vanilla JS.', link: './etch-a-sketch/'}
+			{ name: 'To-Do List', description: 'A full stack To-Do list application built with Express, Vue, and MySQL.', link: 'https://todo-app-cmacswan.herokuapp.com/' },
+			{ name: 'Shopping List', description: 'A shopping list tool made with Vue JS.', link: './shopping_list/' },
+			{ name: 'My Library', description: 'A tool to help keep track of a library of books. Built with vanilla JS.', link: './my_library/' },
+			{ name: 'Tic Tac Toe', description: 'Tic-tac-toe game. Vanilla JS.', link: './tic_tac_toe/' },
+			{ name: 'Calculator', description: 'Simple calculator. Vanilla JS.', link: './calculator/' },
+			{ name: 'Framework', description: 'CSS grid framework.', link: './framework_project/' },
+			{ name: 'Etch-A-Sketch', description: 'A browser etch-a-sketch made with vanilla JS.', link: './etch-a-sketch/'}
 		]
 	}
 })
